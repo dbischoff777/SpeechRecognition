3af9b4e8c16a2412251d0d153452b7d71c6771b5
dbischoff777 <dbischoff777@gmail.com>
2022-04-19 22:00:49 +0200

    init

---
+++ main.py
@@ -0,0 +1,64 @@
+import speech_recognition as sr
+import gtts
+from playsound import playsound
+import os
+from notion import NotionClient
+from datetime import datetime
+
+r= sr.Recognizer()
+
+token = ""
+db_id = ""
+
+client = NotionClient(token, db_id)
+
+ACTIVATION_COMMAND = "hey sam"
+
+def get_audio():
+    with sr.Microphone() as source:
+        print("Say something")
+        audio = r.listen(source)
+    return audio
+
+def audio_to_text(audio):
+    text = ""
+    try:
+        text = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Speech recognition could not understand audio")
+    except sr.RequestError:
+        print("could not request results from API")
+    return text
+
+def play_sound(text):
+    try:
+        tts = gtts.gTTS(text)
+        tempfile = "./temp.mp3"
+        tts.save(tempfile)
+        playsound(tempfile)
+        os.remove(tempfile)
+    except AssertionError:    
+        print("could not play sound")
+
+if __name__ == "__main__":
+
+    while True:
+        a = get_audio()
+        command = audio_to_text(a)
+
+        if ACTIVATION_COMMAND in command.lower():
+            print("activate")
+            #trigger sound to signal activation
+            play_sound("What can I do for you?")
+
+            note = get_audio()
+            note = audio_to_text(note)
+
+            if note:
+                play_sound(note)
+
+                now = datetime.now().astimezone().isoformat()
+                res = client.create_page(note, now, status="Active")
+                if res.status_code == 200:
+                    play_sound("Stored new entry")
+
---
+++ notion.py
@@ -0,0 +1,50 @@
+import json
+import requests
+
+class NotionClient:
+
+    def __init__(self, token, db_id) -> None:
+        self.db_id = db_id
+        
+        self.heards = {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json",
+            "Notion-Version": "2021-08-16"
+        }
+
+    def create_page(self, description, date, status):
+        create_url = "https://api.notion.com/v1/pages"
+
+        data = {
+        "parent": { "database_id": self.db_id },
+        "properties": {
+            "Description": {
+                "title": [
+                    {
+                        "text": {
+                            "content": description
+                        }
+                    }
+                ]
+            },
+            "Date": {
+                "date": {
+                            "start": date,
+                            "end": None
+                        }
+            },
+            "Status": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": status
+                        }
+                    }
+                ]
+            }
+        }}
+
+        data = json.dumps(data)
+        res = requests.post(create_url, headers=self.headers, data=data)
+        print(res.status_code)
+        return res
